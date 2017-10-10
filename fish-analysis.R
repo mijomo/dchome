638ac9c9f5ba536c34d11ddfdb1839d1ce1cc71f
Michael Moreau <michaeljmoreau@Michaels-MacBook-Pro.local>
2017-10-10 14:24:34 -0400

    Updated file naming convention to prepare for more files.

--- fish-analysis.R
+++ fish-analysis.R
@@ -1,7 +1,7 @@
 library(dplyr)
 
-fish_data <- read.csv("raw_data/Gaeta_etal_CLC_data.csv") %>% 
-  filter(scalelength < 1)
+fish_data <- read.csv("raw_data/Gaeta_etal_CLC_data_1.csv") %>% 
+  filter(scalelength > 1)
 
 nrow(fish_data)
 
