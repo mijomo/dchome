decd190d3c2d8f7072c2b237f0365777c0e6c64e
Michael Moreau <michaeljmoreau@Michaels-MacBook-Pro.local>
2017-10-10 14:21:18 -0400

    Removed scalelength < 1 due to device limitations.

--- fish-analysis.R
+++ fish-analysis.R
@@ -1,6 +1,9 @@
 library(dplyr)
 
-fish_data <- read.csv("raw_data/Gaeta_etal_CLC_data.csv")
+fish_data <- read.csv("raw_data/Gaeta_etal_CLC_data.csv") %>% 
+  filter(scalelength < 1)
+
+nrow(fish_data)
 
 fish_data_cat <- fish_data %>%
   mutate(length_cat = ifelse(length > 300, "big", "small"))
