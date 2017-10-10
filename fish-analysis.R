fafdc306a0855f12ec2621ca9e4aff904461afe2
Michael Moreau <michaeljmoreau@Michaels-MacBook-Pro.local>
2017-10-10 14:47:07 -0400

    New scatterplot showing relationship between length and scalelength.

--- fish-analysis.R
+++ fish-analysis.R
@@ -1,12 +1,13 @@
 library(dplyr)
+library(ggplot2)
 
 fish_data <- read.csv("raw_data/Gaeta_etal_CLC_data_1.csv") %>% 
   filter(scalelength > 1)
 
-nrow(fish_data)
-
 fish_data_cat <- fish_data %>%
   mutate(length_cat = ifelse(length > 300, "big", "small"))
 
-
+# Exercuse 5.1_relationship between body length and size of scales across different lakes
+ex5.1 <- ggplot(data = fish_data) +
+  geom_point(mapping = aes(x = length, y = scalelength, color = lakeid))
 
